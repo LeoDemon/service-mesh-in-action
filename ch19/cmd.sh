7dd51842c8dd81bf83815dd121454d115658f096
Demon.Lee <jwkljh@163.com>
2023-02-13 13:35:30 +0800

    ch19: istio retries test using  filed

--- ch19/cmd.sh
+++ ch19/cmd.sh
@@ -1,3 +1,6 @@
 # use curl access ngx-istio proxy service
 curl -w "@curl-format.txt" -i http://ngx-istio:8080/
 
+# check pod proxy routes
+istioctl pc route student-545cff6886-4t955.istio-dev -o yaml --name 8080 > proxy-route-status.yaml
+
